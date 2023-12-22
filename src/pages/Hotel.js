7229d3b1afaec88b5f888578a630d40d7c6d565c
khangnguyen213 <khangnguyenm01@gmail.com>
2023-12-22 14:01:21 +0700

    fix

--- src/pages/Hotel.js
+++ src/pages/Hotel.js
@@ -63,7 +63,7 @@ const Hotel = () => {
   };
 
   useEffect(() => {
-    fetch(`'${Global.BASE_BACKEND_API}/hotel`)
+    fetch(`${Global.BASE_BACKEND_API}/hotel`)
       .then((res) => res.json())
       .then((data) => setHotelsArr(data))
       .catch((err) => console.log(err));
